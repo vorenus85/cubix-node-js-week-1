2ed0db2c0d7c1817617b45fee608875c62de59b2
Janos Perge <janos.perge@gmail.com>
2024-03-04 11:30:24 +0100

    fix order of perations

--- exercise_2.js
+++ exercise_2.js
@@ -10,14 +10,14 @@ function mul(numA, numB) {
   return numA * numB;
 }
 
-// (5+4) - (3 * (2+1)) - 6
+// (5+4) - (3 * (2+1)) - 6 = -6
 
 async function calculate() {
   const firstOperation = await add(2, 1); //2+1
   const secondOperation = mul(3, firstOperation); // 3 * (2+1)
-  const thirdOperation = await sub(secondOperation, 6); //(3 * (2+1)) - 6
-  const fourthOperation = await add(5, 4); // 5+4
-  const result = await sub(fourthOperation, thirdOperation);
+  const thirdOperation = await add(5, 4); // 5+4
+  const fourthOperation = await sub(thirdOperation, secondOperation); // (5+4) - (3 * (2+1))
+  const result = await sub(fourthOperation, 6);
 
   console.log(result);
 }
